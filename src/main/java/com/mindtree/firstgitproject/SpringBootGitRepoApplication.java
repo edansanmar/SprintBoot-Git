804f0f6830f2d00155e9080a6022835924af1ce6
Akhil <kayshikacode@gmail.com>
2022-08-09 22:25:39 +0530

    First Project Of SpringBoot Using Git

---
+++ src/main/java/com/mindtree/firstgitproject/SpringBootGitRepoApplication.java
@@ -0,0 +1,13 @@
+package com.mindtree.firstgitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindtree/firstgitproject/SpringBootGitRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.firstgitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
